17f98a9f63b5b0b8f8bdcf92acfd47c95d856363
beomsuong <beomsuong0406@gmail.com>
2024-01-08 22:40:13 +0900

    스켈레톤 추가

---
+++ lib/home_screen_shimmer.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeScreenShimmer extends StatefulWidget {
+  const HomeScreenShimmer({super.key});
+
+  @override
+  State<HomeScreenShimmer> createState() => _HomeScreenShimmerState();
+}
+
+class _HomeScreenShimmerState extends State<HomeScreenShimmer> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
